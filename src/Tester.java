38a73b3f16588e1012c3e1ded641517277cf6176
Steven Arroyo <arroyo47@students.rowan.edu>
2021-03-02 23:05:52 -0500

    added check for Prepared Statements in algorithm

--- src/Tester.java
+++ src/Tester.java
@@ -10,12 +10,24 @@ public class Tester {
 				"uPass = getRequestString(\"userpassword\");\r\n" + 
 				"\r\n" + 
 				"sql = 'SELECT * FROM Users WHERE Name =\"' + uName + '\" AND Pass =\"' + uPass + '\"'"
-				+ "\n SELECT CustomerName, City FROM Customers;");
+				+ "\n SELECT CustomerName, City FROM Customers;"
+				+ "\n String Adams = Adams, John\r\n" + 
+				"txtSQL2 = \"SELECT * FROM Users WHERE UserId = \" + txtUserId;\r\n" + 
+				"\r\n" + 
+				"String txtSQL = \"SELECT * FROM Users WHERE UserId = \" + txtUserId;\r\n" + 
+				"\r\n" + 
+				"try (PreparedStatement stmt = conn.prepareStatement(txtSQL)\r\n" + 
+				"\r\n" + 
+				"prepared(txtSQL)\r\n" + 
+				"mysql_stmt_init(txtSQL)\r\n" + 
+				"\r\n" + 
+				"mysql_stmt_init() \r\n" + 
+				"");
 		
 		CodeInterpreter.setLanguage("SQL");
 		if (CodeInterpreter.getLanguage().equals("SQL")) {
 			sqlAnalyzer sqlLang = new sqlAnalyzer(CodeInterpreter.getCode());
-			sqlLang.checkInputValidation();
+			sqlLang.checkSQLInjection();
 		}
 	}
 
