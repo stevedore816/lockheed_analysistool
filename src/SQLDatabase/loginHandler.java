7e21e1c3ba13a49a2ae4736cd801ef785cd989c8
Steven Arroyo <arroyo47@students.rowan.edu>
2021-03-29 21:44:37 -0400

    Got rid of admin user method

--- src/SQLDatabase/loginHandler.java
+++ src/SQLDatabase/loginHandler.java
@@ -25,20 +25,16 @@ public class loginHandler extends SQLHandler {
 	 * user attempting to login into the databases will lock after 3 tries
 	 * no sql needed refer to the sql handler
 	 */
-	public boolean login (int maxAttempts) {
+	public boolean login () {
 			int attempts = 0; 
-			while (attempts != maxAttempts) {
 				if (super.checkUserExists(user) & super.checkPassExists(user,pass)) {
 					System.out.println("You are logged in!");
 					return true;
-				} else {
-					System.out.println("Failed to log you in please retype password and try again!") ; 
-					attempts ++; 
-				}
-			}
-			if (attempts == maxAttempts) {System.out.println("Login not accepted! Please register or wait for an admin");} 
+				} 
+				return false;
+				
+			
 		
-		return false;
 		
 	}
 	
