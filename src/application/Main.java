5f59d1e655df5c4cc759ec3a62c3ecc281d6a24e
Julian <pinoj39@students.rowan.edu>
2021-03-29 22:53:14 -0400

    Please 2

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,18 +1,18 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
-	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("sample.fxml"));
+			Scene scene = new Scene(root, 600, 400);
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
@@ -24,3 +24,4 @@ public class Main extends Application {
 		launch(args);
 	}
 }
+
