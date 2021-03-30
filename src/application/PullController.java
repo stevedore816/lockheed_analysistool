648b666b536279d3175f649c83f3bc27c0d94e68
Julian <pinoj39@students.rowan.edu>
2021-03-29 22:51:47 -0400

    Please

---
+++ src/application/PullController.java
@@ -0,0 +1,26 @@
+package application;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class PullController {
+	
+	
+	@FXML
+	public void returnClick(ActionEvent event) throws IOException {
+		Parent enterTextParent = FXMLLoader.load(getClass().getResource("sample.fxml"));
+		Scene developerScene = new Scene(enterTextParent);
+		Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+		
+		window.setScene(developerScene);
+		window.show();
+	}
+	
+}
